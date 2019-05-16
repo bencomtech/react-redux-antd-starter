d58ffc483feda90cae67e766aa189efce2b9e856
pichid <pichid.de@hengleasing.co.th>
2019-05-16 10:42:49 +0700

    Add files

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Button } from 'antd'
+
+class App extends Component {
+  render() {
+    return (
+      <Button>sdfdsf</Button>
+    )
+  }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+
+import './index.css'
+import App from './components/App'
+import reducers from './reducers'
+import * as serviceWorker from './serviceWorker'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'))
+
+serviceWorker.unregister()
